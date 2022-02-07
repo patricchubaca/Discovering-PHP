a7a7cdf73f06df50c4ad3001c5b9dc0087e9fd2d
Patric Enderson <patric_job@outlook.com>
2022-02-07 17:18:36 -0300

    Atualizações do Repo

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -7,7 +7,7 @@ try {
   // set the PDO error mode to exception
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
   echo "Connected successfully";
-  //Teste
+  //Teste dvohdfov
 
 } catch(PDOException $e) {
   echo "Connection failed: " . $e->getMessage();
