eb1178da3284ec85b22eeccf54ae9ad85b26a786
Patric Enderson <patric_job@outlook.com>
2022-02-07 18:38:56 -0300

    Atualizações no Repositório

--- STOREPHP/core/views/leyouts/inicio.php
+++
@@ -1,9 +0,0 @@
-<div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-
-        <h1>Página inicial da loja</h1>
-
-        </div>
-    </div>
-</div>
--- STOREPHP/core/views/leyouts/loja.php
+++
@@ -1,7 +0,0 @@
-<h1><?= $titulo ?></h1>
-
-<ul>
-<?php foreach($clientes as $cliente):?>
-    <li><?= $cliente ?></li>
-<?php endforeach;?>
-</ul>
