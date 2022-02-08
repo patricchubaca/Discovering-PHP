7d2e5b418d85bc10621e7d8b305f8e30b79e8123
Patric Enderson <patric_job@outlook.com>
2022-02-08 20:54:46 -0300

    Creating :: ConexaoBDMySQL

--- app_help_desk/index.php
+++ app_help_desk/index.php
@@ -3,7 +3,6 @@ $servername = "localhost";
 $username = "root";
 $password = "root";
 $dbname = "BLOG";
-
 try {
   $conn = new PDO("mysql:host=$servername;port=3307;dbname=$dbname", $username, $password);
   $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
