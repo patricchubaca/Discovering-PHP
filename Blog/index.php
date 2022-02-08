4f85b108de50e9fbbdb14f89a00f294b8b2070bb
Patric Enderson <patric_job@outlook.com>
2022-02-08 12:46:55 -0300

    Listing::Contents

--- Blog/index.php
+++ Blog/index.php
@@ -1,8 +1,15 @@
 <?php
-    $artigo = [
-        'link' => 'primeiros_passos_com_html',
-        'titulo' => 'Primeiros passos com JavaScript',
-        'conteudo' => 'Vamos ver os conteudos da linguaguem JavaScript',    
+    $artigos = [
+        [
+            'link' => 'primeiros_passos_com_html',
+            'titulo' => 'Primeiros passos com HTML5',
+            'conteudo' => 'Hoje vamos utilizar a lingueguem de marcação HTML' 
+        ],
+        [
+            'link' => 'logica de programação com JS',
+            'titulo' => 'Primeiros passos com JavaScript',
+            'conteudo' => 'Vamos ver os conteudos da linguaguem JavaScript' 
+        ]
     ];
 ?>
 
@@ -12,20 +19,21 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" type="text/css" href="stylesheet.css" media="screen" />
-    <link rel="stylesheet" type="style.css" href="stylesheet.css" media="screen" />
+    <link rel="stylesheet" type="text/css" href="style.css" media="screen" />
 
     <title>Meu Blog</title>
 </head>
 <body>
-    <h1> <?php echo $artigo['titulo']; ?></h1>
-    <h2>
-        <a href =""><?php echo $artigo['link']; ?>
-    </a>
-    </h2>
-
-    <p><?php echo $artigo['conteudo']; ?></p>
-
+    <?php foreach($artigos as $artigo){ ?>
+    <div id="container">
+        <h1> <?php echo $artigo['titulo']; ?></h1>
+        <h2>
+            <a href =""><?php echo $artigo['link']; ?>
+            </a>
+        </h2>
+        <p><?php echo $artigo['conteudo'] ?></p>
+    </div>
+    <?php } ?>
 </body>
 </html>
 
