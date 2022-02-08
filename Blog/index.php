43ec4a6cdebb9c16b534b0e5cef107758a13e085
Patric Enderson <patric_job@outlook.com>
2022-02-08 11:10:02 -0300

    Criate::Dados PHP in Index

--- Blog/index.php
+++ Blog/index.php
@@ -1,3 +1,36 @@
 <?php
+    $artigo = [
+        'titulo' => 'Primeiros passos com JavaScript',
+        'descricao' => 'Vamos ver os conteudos da linguaguem JavaScript',  
+    ];
+?>
 
-echo "funcionando";
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Meu Blog</title>
+</head>
+<body>
+    <h1> <?php echo $artigo['titulo']; ?> </h1>
+    <h2>
+        <a href ="primeiros-passos-com-sping.html">
+            <?php echo $artigo['descricao']; ?>    
+        </a>
+    </h2>
+
+    <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica</p>
+
+    <h2>
+        <a href ="primeiros-passos-com-sping.html">
+            <?php echo $artigo['descricao']; ?>    
+        </a>
+    </h2>
+
+    <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica</p>
+
+</body>
+</html>
+
