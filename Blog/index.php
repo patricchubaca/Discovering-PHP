bc16392d10b9f0aea17771854b261e1394f0f7f5
Patric Enderson <patric_job@outlook.com>
2022-02-08 11:42:07 -0300

    Body::Dados PHP in Index

--- Blog/index.php
+++ Blog/index.php
@@ -1,7 +1,8 @@
 <?php
     $artigo = [
+        'link' => 'primeiros_passos_com_html',
         'titulo' => 'Primeiros passos com JavaScript',
-        'descricao' => 'Vamos ver os conteudos da linguaguem JavaScript',  
+        'conteudo' => 'Vamos ver os conteudos da linguaguem JavaScript',    
     ];
 ?>
 
@@ -11,26 +12,19 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" type="text/css" href="stylesheet.css" media="screen" />
+    <link rel="stylesheet" type="style.css" href="stylesheet.css" media="screen" />
+
     <title>Meu Blog</title>
 </head>
 <body>
-    <h1> <?php echo $artigo['titulo']; ?> </h1>
+    <h1> <?php echo $artigo['titulo']; ?></h1>
     <h2>
-        <a href ="primeiros-passos-com-sping.html">
-            <?php echo $artigo['descricao']; ?>    
-        </a>
+        <a href =""><?php echo $artigo['link']; ?>
+    </a>
     </h2>
 
-    <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica</p>
-
-    <h2>
-        <a href ="primeiros-passos-com-sping.html">
-            <?php echo $artigo['descricao']; ?>    
-        </a>
-    </h2>
-
-    
-    <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica</p>
+    <p><?php echo $artigo['conteudo']; ?></p>
 
 </body>
 </html>
