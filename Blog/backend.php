6e4b6536fa8916104fd90f98d62dd9eda2f02dcc
Patric Enderson <patric_job@outlook.com>
2022-02-08 14:22:01 -0300

    Sorting out :: BackEnd

---
+++ Blog/backend.php
@@ -0,0 +1,13 @@
+<?php
+ $artigos = [
+    [
+        'link' => 'primeiros_passos_com_html',
+        'titulo' => 'Primeiros passos com HTML5',
+        'conteudo' => 'Hoje vamos utilizar a lingueguem de marcação HTML' 
+    ],
+    [
+        'link' => 'logica de programação com JS',
+        'titulo' => 'Primeiros passos com JavaScript',
+        'conteudo' => 'Vamos ver os conteudos da linguaguem JavaScript' 
+    ]
+];
--- Blog/index.php
+++ Blog/index.php
@@ -1,16 +1,5 @@
 <?php
-    $artigos = [
-        [
-            'link' => 'primeiros_passos_com_html',
-            'titulo' => 'Primeiros passos com HTML5',
-            'conteudo' => 'Hoje vamos utilizar a lingueguem de marcação HTML' 
-        ],
-        [
-            'link' => 'logica de programação com JS',
-            'titulo' => 'Primeiros passos com JavaScript',
-            'conteudo' => 'Vamos ver os conteudos da linguaguem JavaScript' 
-        ]
-    ];
+    require_once "backend.php";
 ?>
 
 <!DOCTYPE html>
