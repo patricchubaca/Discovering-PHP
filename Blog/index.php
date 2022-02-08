6f45d9bf60f150884c45d419aadd8804d2f0294d
Patric Enderson <patric_job@outlook.com>
2022-02-08 11:11:43 -0300

    Creating::Dados PHP in Index

--- Blog/index.php
+++ Blog/index.php
@@ -29,6 +29,7 @@
         </a>
     </h2>
 
+    
     <p> Lorem Ipsum é simplesmente uma simulação de texto da indústria tipográfica e de impressos, e vem sendo utilizado desde o século XVI, quando um impressor desconhecido pegou uma bandeja de tipos e os embaralhou para fazer um livro de modelos de tipos. Lorem Ipsum sobreviveu não só a cinco séculos, como também ao salto para a editoração eletrônica</p>
 
 </body>
