c7278339142217e9d76cd644160febba745d69b2
Patric Enderson <patric_job@outlook.com>
2022-02-09 15:42:04 -0300

    Instructions::SQL

--- BLOG-PDO/Contantes.php
+++ BLOG-PDO/Contantes.php
@@ -1,5 +1,10 @@
 <?php
 //Constantes DB
-
-
+define('MYSQL_SERVER','localhost');
+define('MYSQL_DATABASE','PHP_STORE');
+define('MYSQL_DATABASE','PHP_STORE');
+define('MYSQL_DBNAME','root');
+define('MYSQL_PASS','root');
+define('MYSQL_CHARSET','utf8');
+//
 ?>
