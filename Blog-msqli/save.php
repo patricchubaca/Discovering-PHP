fd8fc6b2916e1af718757ebe83b20cc007e4cd72
Patric Enderson <patric_job@outlook.com>
2022-02-10 09:37:37 -0300

    Refactory :: Project Blog

---
+++ Blog-msqli/save.php
@@ -0,0 +1,13 @@
+<?php 
+     //require_once "backend.php";
+    //require_once "config.php";
+    $mysql = new mysqli('localhost','root', 'root','BLOG');
+    $mysql->set_charset('utf8');
+    $consulta =  $mysql->query('SELECT * FROM artigos');
+    $artigos =   $consulta->fetch_all(MYSQLI_ASSOC);
+
+    if($mysql == TRUE){
+      //  echo "Banco Conectado";
+    }else{
+        echo "Sem sucesso";
+    }
