de7373e560b440dafe61da0e1e22e0c9fca013e7
Patric Enderson <patric_job@outlook.com>
2022-02-22 10:49:03 -0300

    cursoAluraMVC :: GestordeCursos

--- PHP+MySQL/index.php
+++ PHP+MySQL/index.php
@@ -1,45 +1,65 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <script>
-   function button(){
+<?php
 
-    alert('teste');
-    }
-
-</script>
- 
-  <title>Document</title>
-</head>
-<body>
-  <h1>Formulário para cadastro</h1>
-  <form>
-    <label for="fname">Nome do curso:</label><br>
-    <input type="text" id="fname" name="nomeCurso" value=""><br>
-    <label for="lname">Carga horária:</label><br>
-    <input type="text" id="lname" name="cargaHoraria" value=""><br>
-    <label for="lname">Descrição:</label><br>
-    <input type="text" id="lname" name="descricaoCurso" value=""><br><br>
-    <button  onclick="button()">teste</button>
-  </form>
-
-  <?php
-
-  require_once ('./vendor/autoload.php');
+  require_once('./vendor/autoload.php');
 
   use src\Database\ConexaoDB;
 
   $conn = ConexaoDB::conectar();
   $stmt = $conn->prepare("SELECT * FROM artigos");
   $stmts = $stmt->execute();
-  $lista = $stmt->fetchAll(PDO::FETCH_ASSOC);
-  
+  $listas = $stmt->fetchAll(PDO::FETCH_ASSOC);
 
-  
   ?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script>
+    function button() {
+
+      alert('teste');
+    }
+  </script>
+
+  <title>Document</title>
+</head>
+
+<body>
+  <h1>Formulário para cadastro</h1>
+  <form>
+    <label for="fname">Nome do curso:</label><br>
+    <input type="text" id="fname" name="titulo" value=""><br>
+    <label for="lname">Carga horária:</label><br>
+    <input type="text" id="lname" name="conteudo" value=""><br>
+    <label for="lname">Descrição:</label><br>
+    <input type="text" id="lname" name="cargaHoraria" value=""><br><br>
+    <button onclick="button()">teste</button>
+  </form>
+  
+<table class="table table-hover">
+  <thead>
+    <tr>
+      <th scope="col">titulo</th>
+      <th scope="col">conteudo</th>
+      <th scope="col">cargaHoraria</th>  
+    </tr>
+  </thead>
+  <tbody>
+  <?php foreach ($listas as $lista) { ?>
+  <tr>
+    <td><?php echo $lista['titulo']; ?></td>
+    <td><?php echo $lista['conteudo']; ?></td>
+    <td><?php echo $lista['cargaHoraria']; ?></td>
+  </tr>
+  <?php } ?>
+  </tbody>
+</table>
+
 </body>
 
 </html>
