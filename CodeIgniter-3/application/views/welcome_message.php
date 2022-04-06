d4b2a429cd29f3df2bbf4ab1e2d516b8ae87fbc9
Patric Enderson <patric_job@outlook.com>
2022-04-06 11:04:25 -0300

    Renema :: Folder

---
+++ CodeIgniter-3/application/views/welcome_message.php
@@ -0,0 +1,12 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>Welcome to CodeIgniter</title>
+<body>
+	<h1>Hellow World</h1>
+
+</body>
+</html>
