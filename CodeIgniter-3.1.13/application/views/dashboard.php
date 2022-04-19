fa9478f09ce1d9c2292d3856e2e2d4a2f4b6e8d2
patricchubaca <patric_job@outlook.com>
2022-04-19 16:32:05 -0300

    Feat :: Development Form Register

--- CodeIgniter-3.1.13/application/views/dashboard.php
+++ CodeIgniter-3.1.13/application/views/dashboard.php
@@ -9,7 +9,6 @@
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 </head>
 <body>
-
 	<div class="container">
 		<div class="row">
 			<div class="col-6">
@@ -20,40 +19,77 @@
 			</div>
 		</div>
 	</div>
-<div class="container">
-	<table class="table">
-		<thead>
-			<tr>
-				<th scope="col">Nome</th>
-				<th scope="col">E-mail</th>
-				<th scope="col">Cidade</th>
-				<th scope="col">Estado</th>
-				<th scope="col">Ações</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<?php
-				foreach($inscritos as $inscrito){ ?>
-					<th scope="row"><?php echo $inscrito['nome'] ?></th>
-					<td><?php echo $inscrito['email'] ?></td>
-					<td><?php echo $inscrito['cidade'] ?></td>
-					<td><?php echo $inscrito['uf'] ?></td>
-					<td><button type="button" class="btn btn-danger">Deletar</button>
-						<button type="button" class="btn btn-secondary">Editar</button>
+	<div class="container">
+		<table class="table">
+			<thead>
+				<tr>
+					<th scope="col">Nome</th>
+					<th scope="col">E-mail</th>
+					<th scope="col">Cidade</th>
+					<th scope="col">Estado</th>
+					<th scope="col">Ações</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<?php
+					foreach($inscritos as $inscrito){ ?>
+						<th scope="row"><?php echo $inscrito['nome'] ?></th>
+						<td><?php echo $inscrito['email'] ?></td>
+						<td><?php echo $inscrito['cidade'] ?></td>
+						<td><?php echo $inscrito['uf'] ?></td>
+						<td><button type="button" class="btn btn-danger">Deletar</button>
+							<button type="button" class="btn btn-secondary">Editar</button>
+						</td>
+					<?php } ?>
+				</tr>
 
-					</td>
+			</tbody>
+		</table>
 
-				<?php } ?>
-			</tr>
+		<h3> Registro de Usuários </h3>	
 
-		</tbody>
-	</table>	
+		<?php
+			echo form_open(action:"usuarios/novo");
 
+			echo form_label(label_text:"Nome", id:"nome");
 
-</div>
+			echo form_input(array(
+				"name" => "nome",
+				"id" => "nome",
+				"class" => "form-control",
+				"maxlenght" => "255"
+			)); 
 
+			echo form_label(label_text:"E-mail", id:"email");
 
+			echo form_input(array(
+				"name" => "email",
+				"id" => "email",
+				"class" => "form-control",
+				"maxlenght" => "255"
+			)); 
 
+			echo form_label(label_text:"Cidade", id:"cidade");
+
+			echo form_input(array(
+				"name" => "cidade",
+				"id" => "cidade",
+				"class" => "form-control",
+				"maxlenght" => "255"
+			)); 
+
+			echo form_label(label_text:"Estado", id:"estado");
+
+			echo form_input(array(
+				"name" => "estado",
+				"id" => "estado",
+				"class" => "form-control",
+				"maxlenght" => "255"
+			)); 
+
+			echo form_close();
+			?>
+	</div>
 </body>
 </html>
