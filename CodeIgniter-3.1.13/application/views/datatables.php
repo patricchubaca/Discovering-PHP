a9f75b00119b18bdcf793f7d8ce4b12df31d5fb1
patricchubaca <patric_job@outlook.com>
2022-04-24 16:19:05 -0300

    Feat :: DataTables

--- CodeIgniter-3.1.13/application/views/datatables.php
+++ CodeIgniter-3.1.13/application/views/datatables.php
@@ -5,35 +5,45 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Document</title>
 
-	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.11.5/css/jquery.dataTables.css">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
 
-	<script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.11.5/js/jquery.dataTables.js"></script
+	<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.11.5/datatables.min.css"/>
+
+	<script type="text/javascript" src="https://cdn.datatables.net/v/dt/jq-3.6.0/dt-1.11.5/datatables.min.js"></script>
 
 </head>
 <body>
-	<h1>Datatables</h1>
-
-	<table id="exemplo" class="table display">
-		<thead>
-			<tr>
-				<td>Nome</td>
-			</tr>
-			<tbody>
-				<tr>Patric</tr>
-			</tbody>
-		</thead>
 	
-	</table>
-
-	<script
-	src="https://code.jquery.com/jquery-3.6.0.js"
-	integrity="sha256-H+K7U5CnXl1h5ywQfKtSj8PCmoN9aaq30gDh27Xc0jk="
-	crossorigin="anonymous"></script>
+	<div class="container">
+		<div class="container">
+			<h1>DataTables</h1>
+		</div>
+		<table class="table" id="tableUsuarios">
+			<thead>
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">First</th>
+					<th scope="col">Last</th>
+					<th scope="col">Handle</th>
+				</tr>
+			</thead>
+			<tbody>
+				<tr>
+					<th scope="row">1</th>
+					<td>Mark</td>
+					<td>Otto</td>
+					<td>@mdo</td>
+				</tr>
+			</tbody>
+		</table>
+	</div>	
 
 	<script type="text/javascript">
 		
-		$(document).ready( function () {
-			$('#exemplo').DataTable();
+		$(document).ready(function() {
+			$('#tableUsuarios').DataTable( {
+				"ajax": "data/arrays.txt"
+			} );
 		} );
 	</script>
 </body>
