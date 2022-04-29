334de1310dd57300d917fc2dcefa6410ef34ee79
patricchubaca <patric_job@outlook.com>
2022-04-28 21:21:59 -0300

    Feat :: Routes API/JSON/DataTable

--- LaravelDiscovering/resources/views/dashboard.blade.php
+++ LaravelDiscovering/resources/views/dashboard.blade.php
@@ -46,7 +46,6 @@
       $(document).ready(function() {
         $('#tableUsuarios').DataTable( {
             "ajax": "/api/nova",
-
             "columns": [
             { "data": "id" },
             { "data": "nome" },
