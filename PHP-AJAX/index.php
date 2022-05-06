22cb916765847d2beac4ecf1cf8f6f8622e08910
patricchubaca <patric_job@outlook.com>
2022-05-06 08:46:14 -0300

    Feat :: Atts

--- PHP-AJAX/index.php
+++ PHP-AJAX/index.php
@@ -1,3 +1,15 @@
 <?php
 
-echo phpinfo();
+$servername = "localhost:3306";
+$username = "root";
+$password = "root";
+
+
+try {
+  $conn = new PDO("mysql:host=$servername;dbname=MyDB", $username, $password);
+  // set the PDO error mode to exception
+  $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+  echo "Connected successfully";
+} catch(PDOException $e) {
+  echo "Connection failed: " . $e->getMessage();
+}
